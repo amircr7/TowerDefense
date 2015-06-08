5ed8d389b4b535c160110d259f1bbdf36fa6dd16
SimonTod <simon.todeschini@viacesi.fr>
2015-06-08 17:17:56 +0200

    biteu

--- src/carte/Case.java
+++ src/carte/Case.java
@@ -44,3 +44,28 @@ public class Case {
 	}
 	
 }
+
+/*File file = new File("maps/Map1.txt");
+        Scanner inputFile = new Scanner(file);
+        this.cases = new Case[20][20];
+
+
+        int a = 0;
+        while(inputFile.hasNext()){
+            //read line and convert to char[]; store it.
+            array[a] = inputFile.nextLine().toCharArray();
+
+            //if there are more lines, increment the size of the array.
+            if (inputFile.hasNext()){
+                //create a clone array of the same length.
+                char[][] clone = new char[array.length][];
+                //copy elements from the array to the clone. Note that this can be
+                //done by index with a for loop
+                System.arraycopy(array, 0, clone, 0, array.length);
+                //make array a new array with an extra char[]
+                array = new char[array.length + 1][];
+                //copy elements back.
+                System.arraycopy(clone, 0, array, 0, clone.length);
+                a++;
+            }
+        }*/
