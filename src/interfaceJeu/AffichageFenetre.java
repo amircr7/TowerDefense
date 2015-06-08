1bfa928a863647b39dc96ee4bcc7e98d193da717
MaximeThibaut <maxime.thibaut@viacesi.fr>
2015-06-08 15:07:45 +0200

    test

--- src/interfaceJeu/AffichageFenetre.java
+++ src/interfaceJeu/AffichageFenetre.java
@@ -1,6 +1,8 @@
 package interfaceJeu;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
 
 
@@ -10,13 +12,18 @@ public class AffichageFenetre extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private final AffichagePanneau affichagePanneau;
 
+	
+
 	public AffichageFenetre(final int largeur, final int hauteur, final AffichageCases affichageCases[][]) {
 		this.setTitle("TowerDefense");
-		this.setSize(1500, 1000);
-		this.setLocationRelativeTo(null);
+		this.setSize(15000, 1000);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 		this.affichagePanneau = new AffichagePanneau(largeur, hauteur, affichageCases);
 		this.setContentPane(this.affichagePanneau);
+		this.setLayout(new BorderLayout());
+		this.getContentPane().add(new JButton("Tour 1"), BorderLayout.SOUTH);
+		this.getContentPane().add(new JButton("Tour 2"), BorderLayout.SOUTH);
 		this.setVisible(true);
 	}
 	
