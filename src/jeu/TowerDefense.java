cd9f3252e0840719723995192163d3c36451a996
SimonTod <simon.todeschini@viacesi.fr>
2015-06-08 16:22:11 +0200

    prout

--- src/jeu/TowerDefense.java
+++ src/jeu/TowerDefense.java
@@ -1,27 +1,17 @@
 package jeu;
-<<<<<<< HEAD
 
 import interfaceJeu.AffichageFenetre;
 import carte.Case;
-=======
 import interfaceJeu.Affichage;
-
->>>>>>> origin/master
 
 public class TowerDefense {
 	
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
 		//AffichageFenetre fen = new AffichageFenetre(0, 0, null);
-		
 		String carte =Case.recuperationStringCarte(1);
 		Case.StringToTab(carte);
-		
-=======
-		Affichage fen = new Affichage(0, 0, null);
->>>>>>> origin/master
 	}
 
 }
