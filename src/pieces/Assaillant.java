e89c5491053feb51a9a9dbaabe7770cb03fef06d
MaximeThibaut <maxime.thibaut@viacesi.fr>
2015-06-08 11:19:42 +0200

    methode générer random

--- src/pieces/Assaillant.java
+++ src/pieces/Assaillant.java
@@ -1,8 +1,31 @@
 package pieces;
+import java.util.Random;
+
+
 
 public abstract class Assaillant {
 	protected String enemyName;
 	protected int enemyHealth, enemyDamage, enemyRange, enemyResistance, dropRessources;
 	protected double enemySpeed, damageSpeed;
 	protected double slowResistance, freezeResistance;
+	protected static int nombreAssaillant = 5;
+
+
+
+	public Case genererAssaillantRandom()
+	{
+		switch (hasard.nextInt(nombreAssaillant)) {
+		case 0:
+			return new Ddos();
+		case 1:
+			return new Error404();
+		case 2:
+			return new Hack();
+		case 3:
+			return new Trojan();
+		default:
+			return new Virus();
+		}
+	
+	}
 }
