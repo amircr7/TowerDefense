c523e3cbbc7dd8e4ff5644909cd7fefd19918cfd
Smaug01 <julien.coscat@viacesi.fr>
2015-06-08 14:18:34 +0200

    StringToTab

--- src/carte/Case.java
+++ src/carte/Case.java
@@ -3,7 +3,8 @@ package carte;
 import java.sql.*;
 
 public class Case {
-		
+	
+	public char tableauCarte[][] = new char[20][20];
 	public String recuperationStringCarte (int idCarte) {
 		String url = "jdbc:mysql://localhost:3306/towerdefense";
 		String utilisateur = "root";
@@ -12,27 +13,32 @@ public class Case {
 		Connection connexion = null;
 		try {
 		    connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
-		   
-		    /* Création de l'objet gérant les requêtes */
     	    Statement statement = connexion.createStatement();
-    	    /* Exécution d'une requête de lecture */
     	    ResultSet resultat = statement.executeQuery( "SELECT carte FROM carteinit WHERE idCarte = "+idCarte+";" );
     	    resultat.next() ;
     	    carte = resultat.getString("carte");
     	    // System.out.println(carte);
     	    
 		} catch ( SQLException e ) {
-	    /* Gérer les éventuelles erreurs ici */
 		} finally {
     	    if ( connexion != null )
     	        try {
-    	            /* Fermeture de la connexion */
     	            connexion.close();
     	        } catch ( SQLException ignore ) {
-    	            /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
     	        }
     	}		
 		return carte;
+	}
+	
+	
+	
+	public String StringToTab(String carte){
+		
+		for(char i = 0; i != ','; i++){
+			i = tableauCarte[20][20];
+		}
+		return tableauCarte[20][20];
+
 	}
 	
 }
