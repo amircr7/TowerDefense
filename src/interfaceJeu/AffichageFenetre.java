4ff772883f50fe2ed5bd1f7ffa1095d1f388e750
Smaug01 <julien.coscat@viacesi.fr>
2015-06-09 14:06:37 +0200

    Ajout classe fenetre

--- src/interfaceJeu/AffichageFenetre.java
+++ src/interfaceJeu/AffichageFenetre.java
@@ -1,7 +1,13 @@
 package interfaceJeu;
 
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class AffichageFenetre extends JFrame{
@@ -14,20 +20,33 @@ public class AffichageFenetre extends JFrame{
 
 	public AffichageFenetre(final int largeur, final int hauteur, final AffichageCases affichageCases[][]) {
 		this.setTitle("TowerDefense");
-		this.setSize(1500, 1000);
+		this.setSize(500, 500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.affichagePanneau = new AffichagePanneau(largeur, hauteur, affichageCases);
-		this.setContentPane(this.affichagePanneau);
+		
+		JPanel contentGrille = new JPanel();
+		this.setContentPane(contentGrille);
+		this.setLayout(new BorderLayout());
+		contentGrille.setLayout(new GridLayout(20, 20));
+		creationGrille(contentGrille);
 		this.setVisible(true);
 	}
 	
-	
-	public void placerPiece(final AffichagePieces piece) {
-		this.affichagePanneau.placerPiece(piece);
-	}
-
-	public void rafraichir() {
-		this.affichagePanneau.repaint();
+	public static void creationGrille(JPanel contentGrille){
+        JPanel cell[][]= new JPanel[20][20];
+        for(int i=0; i<cell.length; i++){
+            for(int j=0; j<cell.length; j++){
+                cell[i][j]= new JPanel();
+                cell[i][j].setSize(new Dimension(20, 20));
+                if ((i + j) % 2 == 0) {
+                    cell[i][j].setBackground(Color.gray);
+                } else {
+                    cell[i][j].setBackground(Color.white);
+                }
+                contentGrille.add(cell[i][j]);
+            }
+        }
 	}
 }
+	
