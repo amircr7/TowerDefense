d08ab44c3a23fe591378958a53875c770739344f
SimonTod <simon.todeschini@viacesi.fr>
2015-06-09 14:21:22 +0200

    Déplacement fenetreParametre

--- src/jeu/Partie.java
+++ src/jeu/Partie.java
@@ -1,5 +1,77 @@
 package jeu;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class Partie {
-	
+	public void fenetreParametres () {
+		
+		// Création des parametres de la fenetre	
+		JFrame fenetreParametres = new JFrame();
+		fenetreParametres.setTitle("Paramètres");
+		fenetreParametres.setSize(300, 150); //largeur , hauteur
+		fenetreParametres.setLocationRelativeTo(null);
+		fenetreParametres.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetreParametres.setResizable(false);
+		fenetreParametres.setLayout(new BorderLayout());
+			    
+			    
+		//Création choix carte
+		JPanel panCenter = new JPanel();
+		JLabel labelCarte = new JLabel("Choisissez votre carte ---> ");
+		JComboBox<String> choixCarte = new JComboBox<String>();
+		choixCarte.setPreferredSize(new Dimension(100, 20));
+		choixCarte.addItem("Carte 1");
+		choixCarte.addItem("Carte 2");
+		choixCarte.addItem("Carte 3");
+		panCenter.add(labelCarte);
+		panCenter.add(choixCarte);
+		fenetreParametres.getContentPane().add(panCenter, BorderLayout.CENTER);
+		    
+			    
+		//Création zone texte pour nom
+		JPanel panNorth = new JPanel();
+		JLabel labelNom = new JLabel("Entrez votre nom ---> ");
+		JTextField nomUtilisateur = new JTextField("");
+		nomUtilisateur.setPreferredSize(new Dimension(100, 20));
+		panNorth.add(labelNom);
+		panNorth.add(nomUtilisateur);
+		fenetreParametres.getContentPane().add(panNorth, BorderLayout.NORTH);
+		  
+			    
+		// Création boutons valider et annnuler en bas de la fenetre
+		JPanel panSouth = new JPanel();
+		JButton valider = new JButton ("Valider");
+		JButton annuler = new JButton ("Annuler");
+		panSouth.add(valider);
+		panSouth.add(annuler);
+		fenetreParametres.getContentPane().add(panSouth, BorderLayout.SOUTH);
+		valider.addActionListener(new ActionListener() 
+		{	
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			// balance les parametre et ouvre la game
+			}
+		});
+		annuler.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			System.exit(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+			    
+			    
+		// rendre la fenetre visible
+		fenetreParametres.setVisible(true);
+	}
 }
--- src/jeu/TowerDefense.java
+++ src/jeu/TowerDefense.java
@@ -1,8 +1,8 @@
 package jeu;
 
-// import interfaceJeu.AffichageFenetre;
+import interfaceJeu.AffichageFenetre;
 import carte.Case;
-// import interfaceJeu.Affichage;
+import interfaceJeu.Affichage;
 
 public class TowerDefense {
 	
@@ -10,8 +10,9 @@ public class TowerDefense {
 	public static void main(String[] args) {
 		
 		//AffichageFenetre fen = new AffichageFenetre(0, 0, null);
-		String carte =Case.recuperationStringCarte(2);
-		Case.StringToTab(carte);
+		//String carte =Case.recuperationStringCarte(2);
+		//Case.StringToTab(carte);
+		
 	}
 
 }
